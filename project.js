8e73a3cef35beffe0b80a29989f7288fdaa1f2ab
luisfelipebp <luisfelipebarbosa2013@gmail.com>
2022-10-15 20:17:31 -0300

    Ajustes e melhorias v.2

--- project.js
+++ project.js
@@ -1,42 +1,37 @@
 const hamburguerMenu = document.querySelector(".hamburguer-button");
+const closeMenu = document.querySelector(".close-button");
 const menuMobile = document.querySelector(".header-menu-mobile");
 const containerMobile = document.querySelector(".container-menuMobile");
 const menuLinks = document.querySelectorAll(".menu-link");
 
 function abrirMenu() {
-  if (hamburguerMenu && menuMobile && containerMobile) {
-    hamburguerMenu.classList.toggle("ativo");
-    menuMobile.classList.toggle("header-menu-mobile-inativo");
-    containerMobile.classList.toggle("bg-ativo");
-  }
+  hamburguerMenu.classList.toggle("ativo");
+  closeMenu.classList.toggle("inativo");
+  menuMobile.classList.toggle("header-menu-mobile-inativo");
+  containerMobile.classList.toggle("bg-ativo");
 }
 
-function removerFundo({ target }) {
-  if (hamburguerMenu && menuMobile && containerMobile) {
-    if (target === containerMobile) {
-      menuMobile.classList.add("header-menu-mobile-inativo");
-      containerMobile.classList.remove("bg-ativo");
-      hamburguerMenu.classList.remove("ativo");
-    }
-  }
+function removerFundo() {
+  menuMobile.classList.add("header-menu-mobile-inativo");
+  closeMenu.classList.add("inativo");
+  containerMobile.classList.remove("bg-ativo");
+  hamburguerMenu.classList.remove("ativo");
 }
 
 function removerMenuMobile({ target }) {
-  if (hamburguerMenu && menuMobile && containerMobile) {
-    if (target.innerWidth >= 850) {
-      hamburguerMenu.classList.remove("ativo");
-      menuMobile.classList.add("header-menu-mobile-inativo");
-      containerMobile.classList.remove("bg-ativo");
-    }
+  if (target.innerWidth >= 850) {
+    hamburguerMenu.classList.remove("ativo");
+    menuMobile.classList.add("header-menu-mobile-inativo");
+    closeMenu.classList.add("inativo");
+    containerMobile.classList.remove("bg-ativo");
   }
 }
 
-function navegarSecao(event) {
-  if (hamburguerMenu && menuMobile && containerMobile) {
-    hamburguerMenu.classList.remove("ativo");
-    menuMobile.classList.add("header-menu-mobile-inativo");
-    containerMobile.classList.remove("bg-ativo");
-  }
+function navegarSecao() {
+  hamburguerMenu.classList.remove("ativo");
+  menuMobile.classList.add("header-menu-mobile-inativo");
+  containerMobile.classList.remove("bg-ativo");
+  closeMenu.classList.add("inativo");
 }
 
 hamburguerMenu.addEventListener("click", abrirMenu);
